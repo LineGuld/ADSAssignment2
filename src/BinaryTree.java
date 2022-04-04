5caabde8adeb66f986b131ecd21aa7b38fcef048
Line Guld <linekguld@hotmail.com>
2022-04-04 16:05:30 +0200

    Changes by zteph

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class BinaryTree
+public class BinaryTree<E extends Comparable<? super Object>>
 {
   private BinaryTreeNode<E> root;
   private int size;
@@ -18,49 +18,84 @@ public class BinaryTree
 
   public BinaryTreeNode<E> getRoot()
   {
-    //TODO
+    return root;
   }
 
   public void setRoot(BinaryTreeNode<E> node)
   {
-    //TODO
+    if (root != null)
+    {
+      throw new IllegalArgumentException("Tree root is not EMPTY!!! AARRRGGGGH");
+    } else {
+      root = node;
+    }
   }
 
   public boolean isEmpty()
   {
-    //TODO
+    return size() <= 0;
   }
 
-  public  int size()
+  public int size()
+  {
+    return size;
+  }
+
+  public boolean contains(E element)
   {
     //TODO
+    if (root == null) {
+      System.out.println("Trylle trylle ... binær træet er tomt");
+    } else {
+      if (root == element)
+      {
+        return true;
+      }
+      if(element.compareTo(root) < 0)
+      {
+        BinaryTree leftSubtree = new BinaryTree<>(root.getLeftChild());
+        return leftSubtree.contains(element);
+      }
+      if(element.compareTo(root) > 0)
+      {
+        BinaryTree rightSubtree = new BinaryTree<>(root.getRightChild());
+        return rightSubtree.contains(element);
+      }
+      //contains()
+    }
+    return false;
   }
 
-  public boolean contains(Object element)
+  public ArrayList<E> inOrder()
   {
     //TODO
+
+    return null;
   }
 
-  public ArrayList<Object> inOrder()
+  public ArrayList<E> preOrder()
   {
     //TODO
+
+    return null;
   }
 
-  public ArrayList<Object> preOrder()
-  {
+  public ArrayList<E> postOrder(){
     //TODO
+
+    return null;
   }
 
-  public ArrayList<Object> postOrder(){
+  public ArrayList<E> levelOrder(){
     //TODO
-  }
 
-  public ArrayList<Object> levelOrder(){
-    //TODO
+    return null;
   }
 
   public int height()
   {
     //TODO
+
+    return -1;
   }
 }
